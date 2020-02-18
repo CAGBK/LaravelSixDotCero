562ccf04f2ae6da1ada5cce03e74e73e08f49e0b
CAGBK <camiloxd83@hotmail.com>
2020-02-18 09:19:46 -0500

    Change name of models

---
+++ app/Models/CategorySubcategoryDetail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategorySubcategoryDetail extends Model
+{
+    protected $table = "category_subcategory";
+    protected $fillable = [ 'category_id','subcategory_id'];
+    protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+    public function Subcategory()
+    {
+        return $this->belongsTo('App\Models\Subcategory');
+    }
+}
+
+
