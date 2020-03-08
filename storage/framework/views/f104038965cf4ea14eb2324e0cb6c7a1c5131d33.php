b83e1dd23c2c270bdbcdae27a472964b2e1e15f6
ds06 <dsgutierrez06@misena.edu.co>
2020-03-07 19:09:20 -0500

    Se quitan cosas

---
+++ storage/framework/views/f104038965cf4ea14eb2324e0cb6c7a1c5131d33.php
@@ -0,0 +1,64 @@
+<script>
+	var miRuleta;
+	var segmentoSeleccionado;
+	var distnaciaX = 150;
+	var distnaciaY = 50;
+	var ctx ;
+	 miRuleta = new Winwheel({
+	  'canvasId': 'Ruleta',
+	  'numSegments': 4,
+	  'outerRadius': 270,
+	  'innerRadius': 80,
+	  'segments': [
+		{'fillStyle': '#f1c40f', 'text': 'Patologia'},
+		{'fillStyle': '#2ecc71', 'text': 'Producto'},
+		{'fillStyle': '#367e22', 'text': 'Competencia'},
+		{'fillStyle': '#8e44ad', 'text': 'POA'},
+	  ],
+	  'animation':
+	  {
+		'type': 'spinToStop',
+		'duration':4,
+		'spins': 15,
+		'callbackFinished': 'Mensaje()',
+		'callbackAfter': 'dibujarIndicador()' 
+	  }, 
+	});
+	function Mensaje() {
+	  segmentoSeleccionado = miRuleta.getIndicatedSegment();
+	  SonidoFinal();
+	  alert("Elemento seleccionado: " + segmentoSeleccionado.text + "!");
+	  miRuleta.stopAnimation(false);
+	  miRuleta.rotationAngle = 0;
+	  miRuleta.draw();
+	  dibujarIndicador();
+	  
+}
+
+   function dibujarIndicador() {
+	   distnaciaX = 150;
+	   distnaciaY = 50;
+	   ctx = miRuleta.ctx;
+	   ctx.strokeStyle = 'navy';
+	   ctx.fillStyle = '#000000';
+	   ctx.lineWidth = 2;
+	   ctx.beginPath();
+	   ctx.moveTo(distnaciaX + 170, distnaciaY + 5);
+	   ctx.lineTo(distnaciaX + 230, distnaciaY + 5);
+	   ctx.lineTo(distnaciaX + 200, distnaciaY + 40);
+	   ctx.lineTo(distnaciaX + 171, distnaciaY + 5);
+	   ctx.stroke();
+	   ctx.fill();
+   }
+
+
+   
+	dibujarIndicador();
+  var audio = new Audio('alarma.mp3');  // Create audio object and load desired file.
+  function SonidoFinal()
+  {
+	audio.pause();
+	audio.currentTime = 0;
+	audio.play();
+  }
+</script><?php /**PATH C:\Users\dsgut\OneDrive\Escritorio\LaravelSixDotCero\resources\views/scripts/ruleta.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f2b85fe485d649c1735f9ca0a01c6fea0099e45f.php
@@ -0,0 +1,55 @@
+<?php $__env->startSection('template_title'); ?>
+  Crear Desafio
+<?php $__env->stopSection(); ?>
+
+
+<?php $__env->startSection('content'); ?>
+
+<section class="cotainer">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-3 text-center"> 
+        <div class="card bg-warning">
+          <div class="card-body">
+            <h4 class="card-title">Lista de elementos:</h4>  
+            <textarea id="ListaElementos" class="form-control" rows="13">
+              Equipo 1
+              Equipo 2
+              Equipo 3
+              Equipo 4
+              Equipo 5
+              Equipo 6
+              Equipo 7
+              Equipo 10
+            </textarea>
+            <br />
+          </div>
+        </div>
+      </div>
+      <div class="col-7 text-center">
+        <br/>
+        <div id="canvasContainer" onclick="miRuleta.startAnimation()">
+          <canvas id='Ruleta' width='700' height='690'>
+            Canvas not supported, use another browser.
+          </canvas> 
+        </div>
+      </div>
+      <div class="col-2">
+        <br/>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('footer_scripts'); ?>
+
+    <?php echo $__env->make('scripts.Winwheel', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <?php echo $__env->make('scripts.ruleta', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\dsgut\OneDrive\Escritorio\LaravelSixDotCero\resources\views/challenge/ruleta.blade.php ENDPATH**/ ?>
