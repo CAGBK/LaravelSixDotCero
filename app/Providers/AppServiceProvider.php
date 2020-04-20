f58cb59234cd43303cc48e756ef2742595468592
ds06 <dsgutierrez06@misena.edu.co>
2020-04-20 18:15:07 -0500

    Se separa public de codigo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+       app()->bind('path.public', function() {
+    return base_path().'/../public_html';
+}); 
     }
 }
---
+++ storage/framework/views/4d46edd36928e422a28e8da9910cb444c08bacca.php
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+
+	function markNotificationAsRead()
+	{
+		$.get('/markAsRead');
+	}
+	
+</script>	
+ 
+<?php /**PATH C:\Users\dsgut\OneDrive\Escritorio\Prueba\LaravelSixDotCero\resources\views/scripts/main-script.blade.php ENDPATH**/ ?>
