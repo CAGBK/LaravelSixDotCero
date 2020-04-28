b5a39311abafbd1b60739b0ca15e6a4cb64f3355
ds06 <dsgutierrez06@misena.edu.co>
2020-04-28 00:25:16 -0500

    Se ajusta libreria de rulate y se agregan con imagenes

--- resources/views/challenge/ruleta.blade.php
+++ resources/views/challenge/ruleta.blade.php
@@ -29,20 +29,17 @@
       </button>
     </div>
   @endif
-  <div class="container-fluid">
-    <div class="row">
-      <div id="canvasContainer" class="container-canvas" onclick="miRuleta.startAnimation()">
-        <canvas id='Ruleta' width='700' height='690' data-responsiveMinWidth="180" data-responsiveScaleHeight="true" data-responsiveMargin="50">
-            Canvas not supported, use another browser.
-        </canvas> 
-      </div>
-    </div>
+  <div align="center" onclick="miRuleta.startAnimation()">
+    <canvas id="canvas" width="420" height="420">
+      <p align="center">Sorry, your browser doesn't support canvas. Please try another.</p>
+    </canvas>
   </div>
 </section>
 
 @endsection
 @section('footer_scripts')
 
+    @include('scripts.Winwheelmin')
     @include('scripts.Winwheel')
     @include('scripts.ruleta')
 
---
+++ storage/framework/views/5228a01553259ccc53f272f706edccdf2e4a8b4b.php
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+
+	function markNotificationAsRead()
+	{
+		$.get('/markAsRead');
+	}
+	
+</script>	
+ 
+<?php /**PATH C:\laragon\www\LaravelSixDotCero\resources\views/scripts/main-script.blade.php ENDPATH**/ ?>
